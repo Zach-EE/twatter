364f1e974fb8fc37dc59717a37f542c7432761e8
Zach-EE <zachholloway.n@gmail.com>
2022-06-26 15:17:23 -0400

    have login singup forms added and working

--- apps/base/views.py
+++ apps/base/views.py
@@ -1,10 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 def homepage(request):
     return render(request, 'base/homepage.html')
 
-def login(request):
-    return render(request, 'base/login.html')
 
 def signup(request):
-    return render(request, 'base/signup.html')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        
+        if form.is_valid():
+            user = form.save()
+            login (request,user)
+            
+            return redirect('homepage')
+    else:
+        form = UserCreationForm()
+        # context = {
+        #     'form': form,
+        # }
+        
+    return render(request, 'base/signup.html', {'form': form})
--- apps/twatterprofile/models.py
+++ apps/twatterprofile/models.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class TwatterProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    follows = models.ManyToManyField('self', related_name='followed_by', symmetrical=False)
+
+User.twatterprofile = property(lambda u:TwatterProfile.objects.get_or_create(user=u)[0])
