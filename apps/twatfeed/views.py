3cac062394fe758be9963de38f819384a00c6038
Zach-EE <zachholloway.n@gmail.com>
2022-08-17 10:58:14 -0400

    have submittwats functioning as intended, feed populating properly

--- apps/twatfeed/views.py
+++ apps/twatfeed/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 
+from .models import Twat
 @login_required
 def twatfeed(request):
-    return render(request, 'twatfeed/twatfeed.html')
+    userids = [request.user.id]
+    
+    twats = Twat.objects.filter(created_by_id__in=userids)
+    
+    return render(request, 'twatfeed/twatfeed.html', {'twats': twats})
